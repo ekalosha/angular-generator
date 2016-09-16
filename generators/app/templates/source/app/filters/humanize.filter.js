d8e986a69ce9628e57b8876944b657b92bbb8126
Sajera <allsajera@gmail.com>
2016-09-16 17:23:24 +0300

    adding humanize filter

---
+++ generators/app/templates/source/app/filters/humanize.filter.js
@@ -0,0 +1,26 @@
+
+'use strict';
+
+angular
+	// as a deep for root module
+	.module('<%=index.app%>')
+	// filter injector name
+	.filter('humanizeFilter', function () {
+		// filter method
+		return function ( string ) {
+			// filter executer
+			return String( string )
+				// from camel case
+				.replace( /([A-Z])([A-Z])([a-z])|([a-z])([A-Z])/g, '$1$4 $2$3$5' )
+				// .replace(/([a-z]){1,1}([A-Z])/g, function ( sib, f, s ) { return f+" "+s; })
+				// spec
+				.replace(/[_-]+/g, ' ')
+				// normalize
+				.replace(/\s+/g, ' ')
+				// trim
+				.replace(/^\s*|\s*$/g, '')
+				// capitalize
+				.toLowerCase()
+				.replace(/^.{1,1}/, function ( sib ) { return sib.toUpperCase(); });
+		};
+	});
